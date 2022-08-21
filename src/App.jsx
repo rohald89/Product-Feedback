6137c65dead8f0a5295db6009dce57d7aa7354c9
Rohald van Merode <rohald_89@live.nl>
2022-08-21 15:27:02 +0200

    initial commit, set up react app and added tailwind config

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+        <p class="heading-xl">heading 1</p>
+        <p class="heading-lg">heading 2</p>
+        <p class="heading-md">heading 3</p>
+        <p class="heading-sm">heading 4</p>
+        <p class="body-lg">body 1</p>
+        <p class="body-md">body 2</p>
+        <p class="body-bold">body 3</p>
+    </>
+    )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    fontSize: {
+      "body-lg": ["1rem", "1.4375rem"],
+      "body-md": ["0.9275rem", "1.375rem"],
+      "body-sm": ["0.8125rem", "1.1875rem"],
+      xl: ["1.5rem", "2.1875rem"],
+      lg: ["1.25rem", "1.8125rem"],
+      md: ["1.125rem", "1.625rem"],
+      sm: ["0.875rem", "1.5rem"],
+    },
+    letterSpacing: {
+      tightest: "-0.02rem",
+      tighter: "-0.015rem",
+      tight: "-0.01rem",
+    },
+    extend: {
+      fontFamily: {
+        primary: ["Jost", "sans-serif"],
+      },
+      colors: {
+        mainPurple: "#AD1FEA",
+        mainBlue: "#4661E6",
+        lightBlue: "#62BCFA",
+        mainOrange: "#F49F85",
+        veryDarkBlue: "#373F68",
+        darkBlue: "#3A4374",
+        greyBlue: "#647196",
+        lightGrey: "#F7F8FD",
+        grey: "#F2F4FF",
+      },
+    },
+  },
+  plugins: [],
+};
